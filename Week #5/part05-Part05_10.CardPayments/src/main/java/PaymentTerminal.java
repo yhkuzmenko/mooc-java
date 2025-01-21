503dcab1d78b746bc3c95ca61b47f35e538b6ae6
Yevhen Kuzmenko <yhkuzmenko@gmail.com>
2025-01-21 23:03:31 +0200

    Added part05-Part05_10.CardPayments

---
+++ Week #5/part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -0,0 +1,77 @@
+public class PaymentTerminal {
+
+    private double money; // amount of cash
+    private int affordableMeals; // number of sold affordable meals
+    private int heartyMeals; // number of sold hearty meals
+    private static final double AFFORDABLE_PRICE = 2.50; // price of affordable meal
+    private static final double HEARTY_PRICE = 4.30; // price of hearty meal
+
+    public PaymentTerminal() {
+        money = 1000;
+    }
+
+    public double eatAffordably(double payment) {
+        // an affordable meal costs 2.50 euros
+        // increase the amount of cash by the price of an affordable mean and return the
+        // change
+        // if the payment parameter is not large enough, no meal is sold and the method
+        // should return the whole payment
+
+        if (payment >= AFFORDABLE_PRICE) {
+            affordableMeals++;
+            money = money + AFFORDABLE_PRICE;
+            double change = payment - AFFORDABLE_PRICE;
+            return change;
+        }
+        return payment;
+    }
+
+    public double eatHeartily(double payment) {
+        // a hearty meal costs 4.30 euros
+        // increase the amount of cash by the price of a hearty mean and return the
+        // change
+        // if the payment parameter is not large enough, no meal is sold and the method
+        // should return the whole payment
+
+        if (payment >= HEARTY_PRICE) {
+            heartyMeals++;
+            money = money + HEARTY_PRICE;
+            double change = payment - HEARTY_PRICE;
+            return change;
+        }
+        return payment;
+    }
+
+    public boolean eatAffordably(PaymentCard card) {
+        // an affordable meal costs 2.50 euros
+        // if the payment card has enough money, the balance of the card is decreased by
+        // the price, and the method returns true
+        // otherwise false is returned
+
+        if (card.balance() >= AFFORDABLE_PRICE) affordableMeals++;
+        return card.takeMoney(AFFORDABLE_PRICE);
+    }
+
+    public boolean eatHeartily(PaymentCard card) {
+        // a hearty meal costs 4.30 euros
+        // if the payment card has enough money, the balance of the card is decreased by
+        // the price, and the method returns true
+        // otherwise false is returned
+
+        if (card.balance() >= HEARTY_PRICE) heartyMeals++;
+        return card.takeMoney(HEARTY_PRICE);
+    }
+
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        if (sum > 0) {
+            money = money + sum;
+            card.addMoney(sum);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "money: " + money + ", number of sold affordable meals: " + affordableMeals
+                + ", number of sold hearty meals: " + heartyMeals;
+    }
+}
