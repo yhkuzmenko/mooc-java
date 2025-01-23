ab3e4c4273fadc32f901793d4f70ab1d3cab1724
Yevhen Kuzmenko <yhkuzmenko@gmail.com>
2025-01-24 01:41:33 +0200

    Added part06-Part06_06.LongestInCollection

---
+++ Week #6/part06-Part06_06.LongestInCollection/src/main/java/SimpleCollection.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class SimpleCollection {
+
+    private String name;
+    private ArrayList<String> elements;
+
+    public SimpleCollection(String name) {
+        this.name = name;
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(String element) {
+        this.elements.add(element);
+    }
+
+    public ArrayList<String> getElements() {
+        return this.elements;
+    }
+    
+    public String longest() {
+        if (elements.isEmpty()) {
+            return null;
+        }
+
+        String returnObject = elements.get(0);
+
+        for (String element : elements) {
+            if (returnObject.length() < element.length()) {
+                returnObject = element;
+            }
+        }
+
+        return returnObject;
+    }
+}
