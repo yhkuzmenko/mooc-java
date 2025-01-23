934b8cf9cbc850563992e71e8b2b8a8d2aba22a4
Yevhen Kuzmenko <yhkuzmenko@gmail.com>
2025-01-23 12:41:51 +0200

    Added part05-Part05_17.Money

---
+++ Week #5/part05-Part05_17.Money/src/main/java/MainProgram.java
@@ -0,0 +1,19 @@
+public class MainProgram {
+
+    public static void main(String[] args) {
+        Money a = new Money(10, 0);
+        Money b = new Money(3, 50);
+
+        Money c = a.minus(b);
+
+        System.out.println(a); // 10.00e
+        System.out.println(b); // 3.50e
+        System.out.println(c); // 6.50e
+
+        c = c.minus(a);
+
+        System.out.println(a); // 10.00e
+        System.out.println(b); // 3.50e
+        System.out.println(c); // 0.00e
+    }
+}
